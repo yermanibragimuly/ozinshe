c88f320dd5a898ba5ce1d315fffbb018ac269af2
yermanibragimuly <yerman.ibragimuly@gmail.com>
2025-08-23 18:21:07 +0500

    Добавил OnboardingViewController

---
+++ ozinshe/OnboardingCell.swift
@@ -0,0 +1,13 @@
+//
+//  OnboardingCell.swift
+//  ozinshe
+//
+//  Created by Yerman Ibragimuly on 22.08.2025.
+//
+
+import UIKit
+
+class OnboardingCell: UICollectionViewCell {
+    let identifier = "OnboardingCell"
+    
+}
---
+++ ozinshe/OnboardingViewController.swift
@@ -0,0 +1,66 @@
+//
+//  OnboardingViewController.swift
+//  ozinshe
+//
+//  Created by Yerman Ibragimuly on 22.08.2025.
+//
+
+import UIKit
+import SnapKit
+
+class OnboardingViewController: UIViewController {
+    
+    var arraySlides = [["firstSlides", "ÖZINŞE-ге қош келдің!", "Фильмдер, телехикаялар, ситкомдар, анимациялық жобалар, телебағдарламалар мен реалити-шоулар, аниме және тағы басқалары"], ["secondSlides", "ÖZINŞE-ге қош келдің!", "Кез келген құрылғыдан қара Сүйікті фильміңді  қосымша төлемсіз телефоннан, планшеттен, ноутбуктан қара"], ["thirdSlides", "ÖZINŞE-ге қош келдің!", "Тіркелу оңай. Қазір тіркел де қалаған фильміңе қол жеткіз"]]
+    
+    var currentPage = 0 {
+        didSet {
+            pageControl().currentPage = currentPage
+        }
+    }
+    
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 0
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(OnboardingCell.self, forCellWithReuseIdentifier: "OnboardingCell")
+        collectionView.backgroundColor = UIColor(named: " ")
+        collectionView.contentInsetAdjustmentBehavior = .never
+        collectionView.isPagingEnabled = true
+        collectionView.isScrollEnabled = true
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.showsHorizontalScrollIndicator = false
+        
+        return collectionView
+    }()
+    
+    let pageControl = {
+        let pc = UIPageControl()
+        pc.numberOfPages = 3
+        pc.tintColor = .black
+        pc.currentPage = 0
+        pc.currentPageIndicatorTintColor = UIColor(red: 0.7, green: 0.46, blue: 0.97, alpha: 1)
+        pc.contentVerticalAlignment = .center
+        pc.contentHorizontalAlignment = .center
+        
+        return pc
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
